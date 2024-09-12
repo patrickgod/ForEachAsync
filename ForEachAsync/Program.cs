8d8f0a4a32f89ad306766aa5d63121ea60d4b94d
Patrick <patrick@godknows.de>
2024-09-12 14:57:51 +0200

    Add project files.

---
+++ ForEachAsync/Program.cs
@@ -0,0 +1,32 @@
+﻿async Task<List<string>> FetchDataAsync(List<int> ids)
+{
+    var results = new List<string>();
+
+    await Parallel.ForEachAsync(ids, async (id, token) =>
+    {
+        var result = await SimulateFetchFromDatabase(id);
+        Console.WriteLine(result);
+        results.Add(result);
+    });
+
+    //foreach (var id in ids)
+    //{
+    //    var result = await SimulateFetchFromDatabase(id);
+    //    Console.WriteLine(result);
+    //    results.Add(result);
+    //}
+
+    return results;
+}
+
+async Task<string> SimulateFetchFromDatabase(int id)
+{
+    await Task.Delay(1000);
+    var result = $"Data for ID {id}";
+    return result;
+}
+
+var ids = new List<int> { 1, 2, 3, 4 };
+var fetchedData = await FetchDataAsync(ids);
+
+Console.WriteLine($"Fetched Data Count: {fetchedData.Count}");
